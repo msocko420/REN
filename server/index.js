af9235a730c94f132dcccd6590cf739e00c1843b
msocko420 <122328760+msocko420@users.noreply.github.com>
2023-09-25 13:44:19 -0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ import cors from 'cors';
 
 import dalleRoutes from './routes/dalle.routes.js';
 import userRoutes from './user.routes.js'; // Import user routes using ES6 import syntax
+import feedbackRoutes from './feedback.routes.js'; // Import feedback routes
 
 dotenv.config();
 
@@ -18,8 +19,11 @@ app.use('/api/v1/dalle', express.json({ limit: "150mb" }), dalleRoutes);
 // For the /api/user endpoint, don't apply the express.json() middleware globally
 app.use('/api/user', userRoutes); // Add user routes
 
+// Add feedback routes with express.json() middleware
+app.use('/api/feedback', express.json(), feedbackRoutes);
+
 app.get('/', (req, res) => {
-  res.status(200).json({ message: "Hello from DALL.E" })
+  res.status(200).json({ message: "Hello from DALL.E" });
 });
 
 app.listen(8080, () => console.log('Server has started'));
